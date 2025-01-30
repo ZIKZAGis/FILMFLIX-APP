7e840eb14e76572377b524bca09e45dc797e0ab8
Evgeniy <nikolaev_zheka-zik@bk.ru>
2025-01-30 15:35:06 +0300

    добавляет navbar

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import { AppBar, Box, Container, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Slide, Toolbar, Typography, useScrollTrigger, Link, Divider } from '@mui/material';
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Link as RouterLink } from 'react-router-dom';
+import {TOP_LISTS, MOVIE_LISTS, iconComponents} from '../../../constants'
+
+// Верная ли типизация?
+// Почему при запуске проекта, import {TOP_LISTS, iconComponents} from '../../../constants' - подсвечивается как ошибка 'не удалось найти файл обьявления модуля...'?
+
+type IconName = keyof typeof iconComponents;
+
+interface IconProps {
+    iconName: IconName;
+}
+
+interface ListItemIf {
+    title: string;
+    icon: string;
+    url: string;
+}
+
+const Icon = ({iconName}: IconProps) => {
+    const IconComponent = iconComponents[iconName]
+    return <IconComponent />;
+};
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setIsOpen(prevState => !prevState);
+    };
+    const trigger = useScrollTrigger({
+        target: window,
+    });
+
   return (
-    <div>Navbar</div>
+    <Slide appear={false} direction="down" in={!trigger}>
+        <AppBar>
+            <Container maxWidth="lg">
+                <Toolbar>
+                    <IconButton color="inherit" onClick={handleDrawerToggle}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Drawer open={isOpen} onClose={handleDrawerToggle}>
+                        <Box sx={{width: 400}} onClick={handleDrawerToggle}>
+                            <List>
+                                {TOP_LISTS.map((item: ListItemIf) => (
+                                    <Link key={item.title} component={RouterLink} to={item.url}>
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <Icon iconName={item.icon}/>
+                                                </ListItemIcon>
+                                                <ListItemText primary={item.title} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                ))}
+                            </List>
+                            <Divider/>
+                            <List>
+                                {MOVIE_LISTS.map((item: ListItemIf) => (
+                                    <Link key={item.title} component={RouterLink} to={item.url}>
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <Icon iconName={item.icon}/>
+                                                </ListItemIcon>
+                                                <ListItemText primary={item.title} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                ))}
+                            </List>
+                        </Box>
+                    </Drawer>
+                    <Typography 
+                        sx={{color: 'white', textDecoration: 'none'}} 
+                        component={RouterLink } 
+                        to="/"
+                        variant='h5'
+                    >
+                        FilmFlix
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    </Slide>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,95 @@
+import {
+    AutoAwesome,
+    StarPurple500,
+    Bloodtype,
+    MenuBook,
+    FamilyRestroom,
+    VolunteerActivism,
+    MoodBad,
+    Pool,
+    LiveTv,
+    LocalMovies,
+    Reorder,
+    Fort,
+} from '@mui/icons-material'
+
+export const iconComponents = {
+    AutoAwesome,
+    StarPurple500,
+    Bloodtype,
+    MenuBook,
+    FamilyRestroom,
+    VolunteerActivism,
+    MoodBad,
+    Pool,
+    LiveTv,
+    LocalMovies,
+    Reorder,
+    Fort,
+};
+
+export const TOP_LISTS = [
+    {
+        title: 'ТОП 100 популярных фильмов',
+        icon: 'AutoAwesome',
+        url: '/popular'
+    },
+    {
+        title: 'ТОП 250 лучших фильмов',
+        icon: 'StarPurple500',
+        url: '/best'
+    },
+    {
+        title: 'Вампиры',
+        icon: 'Bloodtype',
+        url: '/vampire'
+    },
+    {
+        title: 'Комиксы',
+        icon: 'MenuBook',
+        url: '/comics'
+    },
+    {
+        title: 'Семейный',
+        icon: 'FamilyRestroom',
+        url: '/family'
+    },
+    {
+        title: 'Романтика',
+        icon: 'VolunteerActivism',
+        url: '/romantic'
+    },
+    {
+        title: 'Зомби',
+        icon: 'MoodBad',
+        url: '/zombie'
+    },
+    {
+        title: 'Катастрофы',
+        icon: 'Pool',
+        url: '/catastrophe'
+    },
+    {
+        title: 'Популярные сериалы',
+        icon: 'LiveTv',
+        url: '/popular-serials'
+    },
+];
+
+export const MOVIE_LISTS = [
+    {
+        title: 'Фильмы',
+        icon: 'LocalMovies',
+        url: '/films'
+    },
+    {
+        title: 'Сериалы',
+        icon: 'Reorder',
+        url: '/serials'
+    },
+    {
+        title: 'Мультфильмы',
+        icon: 'Fort',
+        url: '/cartoons'
+    },
+];
